227289f8048278993c0e87c5227c98d1486abe1a
Jada Grant <jadagrant@fb.com>
2019-07-09 16:53:27 -0700

    Updated: Login feature, new icon, Posts are successful

--- app/src/main/java/com/example/instagram/HomeActivity.java
+++ app/src/main/java/com/example/instagram/HomeActivity.java
@@ -1,32 +1,100 @@
 package com.example.instagram;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import com.example.instagram.model.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+    private static final String imagePath = "Macintosh HD/Users/jadagrant/Documents/sunflower.jpeg";
+    private EditText descriptionInput;
+    private Button createButton;
+    private Button refreshButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        descriptionInput = findViewById(R.id.description_et);
+        createButton = findViewById(R.id.create_btn);
+        refreshButton = findViewById(R.id.refresh_btn);
+        loadToPosts();
+
+        createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                final String description = descriptionInput.getText().toString();
+                final ParseUser user = ParseUser.getCurrentUser();
+
+                final File file = new File(imagePath);
+                final ParseFile parseFile = new ParseFile(file);
+
+                createPost(description, parseFile, user);
             }
         });
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadToPosts();
+            }
+        });
+
     }
 
+    private void createPost(String description, ParseFile imageFile, ParseUser user){
+        final Post newPost = new Post();
+        newPost.setDescription(description);
+        newPost.setImage(imageFile);
+        newPost.setUser(user);
+
+        newPost.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null){
+                    Log.d("HomeActivity", "Post Success");
+                }
+                else {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    private void loadToPosts(){
+        final Post.Query postQuery = new Post.Query();
+        postQuery.getTop().withUser();
+
+        postQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); i++) {
+                        Log.d("HomeActivity", "Post[" + i + "] = " + objects.get(i).getDescription()
+                                + "\nUsername = " + objects.get(i).getUser().getUsername());
+                    }
+
+                } else {
+                    Log.e("HomeActivity", "Failed");
+                }
+            }
+        });
+
+    }
 }
+
--- app/src/main/java/com/example/instagram/model/Post.java
+++ app/src/main/java/com/example/instagram/model/Post.java
@@ -3,6 +3,7 @@ package com.example.instagram.model;
 import com.parse.ParseClassName;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 @ParseClassName("Post")
@@ -28,5 +29,21 @@ public class Post extends ParseObject {
     }
     public void setUser(ParseUser user){
         put(KEY_USER, user);
+    }
+
+    public static class Query extends ParseQuery<Post> {
+
+        public Query() {
+            super(Post.class);
+        }
+       public Query getTop(){
+            setLimit(20);
+            return this;
+       }
+
+       public Query withUser(){
+            include("user");
+            return this;
+       }
     }
 }
