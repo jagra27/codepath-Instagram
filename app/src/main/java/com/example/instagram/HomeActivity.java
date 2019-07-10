7594f2fcf646e2274d014f0bcb96601489953adb
Jada Grant <jadagrant@fb.com>
2019-07-10 15:23:25 -0700

    Updates: User can stay signed in, Query for Posts, working on camera feature now

--- app/src/main/java/com/example/instagram/HomeActivity.java
+++ app/src/main/java/com/example/instagram/HomeActivity.java
@@ -1,28 +1,41 @@
 package com.example.instagram;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.example.instagram.model.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
-import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import java.io.File;
 import java.util.List;
 
+import static com.parse.ParseUser.logOut;
+
 public class HomeActivity extends AppCompatActivity {
     private static final String imagePath = "Macintosh HD/Users/jadagrant/Documents/sunflower.jpeg";
     private EditText descriptionInput;
     private Button createButton;
     private Button refreshButton;
+    private Button logoutButton;
+    private ImageView ivPostImage;
+    private Button captureButton;
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
+    private File photoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +45,10 @@ public class HomeActivity extends AppCompatActivity {
         descriptionInput = findViewById(R.id.description_et);
         createButton = findViewById(R.id.create_btn);
         refreshButton = findViewById(R.id.refresh_btn);
+        logoutButton = findViewById(R.id.logout_btn);
+        ivPostImage = findViewById(R.id.ivPostImage);
+        captureButton = findViewById(R.id.capture_btn);
+
         loadToPosts();
 
         createButton.setOnClickListener(new View.OnClickListener() {
@@ -40,10 +57,10 @@ public class HomeActivity extends AppCompatActivity {
                 final String description = descriptionInput.getText().toString();
                 final ParseUser user = ParseUser.getCurrentUser();
 
-                final File file = new File(imagePath);
-                final ParseFile parseFile = new ParseFile(file);
+               // final File file = new File(imagePath);
+               // final ParseFile parseFile = new ParseFile(file);
 
-                createPost(description, parseFile, user);
+                createPost(description, user);
             }
         });
 
@@ -53,13 +70,69 @@ public class HomeActivity extends AppCompatActivity {
                 loadToPosts();
             }
         });
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("HomeActivity", "Button Clicked");
+                logOut();
+                final Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+
+            }
+        });
+
+        captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
 
     }
 
-    private void createPost(String description, ParseFile imageFile, ParseUser user){
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(HomeActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "HomeActivity");
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d("HomeActivity", "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    private void createPost(String description, ParseUser user){
         final Post newPost = new Post();
         newPost.setDescription(description);
-        newPost.setImage(imageFile);
+        //newPost.setImage(imageFile);
         newPost.setUser(user);
 
         newPost.saveInBackground(new SaveCallback() {
--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -12,12 +12,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText usernameInput;
     private EditText passwordInput;
     private Button  loginBtn;
+    private Button signBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +30,61 @@ public class MainActivity extends AppCompatActivity {
         usernameInput = findViewById(R.id.etUsername);
         passwordInput = findViewById(R.id.etPassword);
         loginBtn = findViewById(R.id.btnLogin);
+        signBtn = findViewById(R.id.btnSign);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               final String username = usernameInput.getText().toString();
-               final String password = passwordInput.getText().toString();
+                final String username = usernameInput.getText().toString();
+                final String password = passwordInput.getText().toString();
 
-               login(username, password);
+                login(username, password);
             }
         });
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+
+        final String password = passwordInput.getText().toString();
+        if (currentUser != null) {
+            // do stuff with the user
+            Log.d("LoginActivity", "Success");
+            final Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+
+            signBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser user = new ParseUser();
+
+                final String username = usernameInput.getText().toString();
+                final String password = passwordInput.getText().toString();
+
+                user.setUsername(username);
+                user.setPassword(password);
+
+
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Log.d("SignUpActivity", "Sign Up Success");
+                            final Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+
     }
 
     private void login(String username, String password){
@@ -52,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else{
                    Log.e("LoginActivity", "Login Failure");
+                   e.printStackTrace();
                 }
             }
         });
